e0dfde2937a5e4bd06338a3ac7b6bb81412655e1
Erin Templeton <erindtempleton@gmail.com>
2024-04-23 14:51:34 -0700

    Add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 function App() {
+
+  const [ imageUpload, setImageUpload ] = useState();
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {/* Home Page */}
+          <Route path='/'></Route>
+
+          {/* My Closet */}
+          <Route path='/closets/myCloset'></Route>
+
+          {/* Add item to my closet */}
+          <Route path='/add'></Route>
+          
+          {/* Search Form */}
+          <Route path='/search'></Route>
+
+          {/* Items List */}
+          <Route path='/items'></Route>
+
+          {/* List friends' closets */}
+          <Route path='/closets'></Route>
+
+          {/* Closet */}
+          <Route path='/closets/:userId'></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
